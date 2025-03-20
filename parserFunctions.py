60fc3dc9384d055fc371caa8ed7ebe07c7366655
anhhdangg01 <dangaq1@uci.edu>
2025-03-19 22:21:11 -0700

    added newlines

--- parserFunctions.py
+++ parserFunctions.py
@@ -16,10 +16,11 @@ def insertViewerParser(uid, email, nickname, street, city, state, zip, genres, j
 	result += uid + ', ' + email + ', ' + nickname + ', ' + street + ', ' + city + ', ' + state + ', ' + zip + ', ' + genres + ', ' + joined_date + ', ' + first + ', ' + last + ', ' + subscription + ');'
 	return result
 	
+
 def deleteViewerParser(uid):
 	return 'DELETE FROM Viewers V\nWHERE V.uid = ' + uid + ';'
 	
-	
+
 def selectGenresParser(uid, db):
 	cursor = db.cursor()
 	cursor.execute('SELECT U.genres\nFROM Users U\nWHERE U.uid = ' + uid + ';')
@@ -27,30 +28,38 @@ def selectGenresParser(uid, db):
 	cursor.close()
 	return query[0]
 
+
 def addGenreParser(uid, genre, old_genres):
 	currGenres = old_genres + '; ' + genre
 	return 'UPDATE Users\nSET genres = ' + currGenres + '\nWHERE uid = ' + uid + ';'
 	
+	
 def insertMovieParser(rid, website_url):
 	return 'INSERT INTO Movies\nValues (' + rid + ', ' + website_url + ');'
 	
+
 def insertSessionParser(sid, uid, rid, ep_num, initiate_at, leave_at, quality, device):
 	return 'INSERT INTO Sessions\nVALUES (' + sid + ', ' + uid + ', ' + rid + ', ' + ep_num + ', ' + initiate_at + ', '+  leave_at + ', ' + quality + ', ' + device + ');'
 	
+
 def updateReleaseParser(rid, title):
 	return 'UPDATE Releases\nSET title = ' + title + '\nWHERE rid = ' + rid + ';'
 	
+
 def releasesReviewedParser(uid):
 	return 'SELECT DISTINCT R.rid, R.genre, R.title\nFROM Releases R\nWHERE R.uid = ' + uid + \
 	'ORDER BY title ASC;'
 	
+
 def popularReleaseParser(N):
 	return 'SELECT R.rid, R.title, R.reviewCount\nFROM Releases\nORDER BY reviewCount DESC\nLIMIT ' + N + ';'
+
 
 def releaseTitleParser(sid):
 	return 'SELECT S.rid, S.release_title, S.genre, S.video_title, S.ep_num, S.length\n' + \
 	'FROM Sessions S\nWHERE S.sid = ' + sid + ';'
 	
+
 def activeViewersParser(N, start, end):
 	return 'SELECT V.uid, V.firstname, V.lastname\n' + \
 	'FROM Viewers V\n' + \
