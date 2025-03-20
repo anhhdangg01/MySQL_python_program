47e5652619dd5080911194fc9530c1427ec174f8
anhhdangg01 <dangaq1@uci.edu>
2025-03-20 12:11:33 -0700

    Fixed all queries with boolean output

--- parserFunctions.py
+++ parserFunctions.py
@@ -37,20 +37,20 @@ def insertUserParser(db, uid, email, joined_date, nickname, street, city, state,
 		return False
 """
 def deleteViewerParser(uid):
-	return 'DELETE FROM Viewers V\nWHERE V.uid = ' + uid + ';'
+	return 'DELETE FROM viewers V\nWHERE V.uid = ' + uid + ';'
 	
 
 def selectGenresParser(uid, db):
 	cursor = db.cursor()
-	cursor.execute('SELECT U.genres\nFROM Users U\nWHERE U.uid = ' + uid + ';')
+	cursor.execute('SELECT genres\nFROM users\nWHERE uid = ' + uid + ';')
 	query = cursor.fetchone()
 	cursor.close()
 	return query[0]
 
 
 def addGenreParser(uid, genre, old_genres):
-	currGenres = old_genres + '; ' + genre
-	return 'UPDATE Users\nSET genres = ' + currGenres + '\nWHERE uid = ' + uid + ';'
+	currGenres = old_genres + ';' + genre
+	return 'UPDATE users\nSET genres = "' + currGenres + '"\nWHERE uid = ' + uid + ';'
 	
 	
 def insertMovieParser(rid, website_url):
@@ -58,34 +58,34 @@ def insertMovieParser(rid, website_url):
 	
 
 def insertSessionParser(sid, uid, rid, ep_num, initiate_at, leave_at, quality, device):
-	return 'INSERT INTO Sessions\nVALUES (' + sid + ', ' + uid + ', ' + rid + ', ' + ep_num + ', ' + initiate_at + ', '+  leave_at + ', ' + quality + ', ' + device + ');'
+	return 'INSERT INTO sessions\nVALUES (' + sid + ', ' + uid + ', ' + rid + ', ' + ep_num + ', "' + initiate_at + '", "'+  leave_at + '", "' + quality + '", "' + device + '");'
 	
 
 def updateReleaseParser(rid, title):
-	return 'UPDATE Releases\nSET title = ' + title + '\nWHERE rid = ' + rid + ';'
+	return 'UPDATE releases\nSET title = "' + title + '"\nWHERE rid = ' + rid + ';'
 	
 
 def releasesReviewedParser(uid):
-	return 'SELECT DISTINCT R.rid, R.genre, R.title\nFROM Releases R\nWHERE R.uid = ' + uid + \
+	return 'SELECT DISTINCT rid, genre, title\nFROM releases R\nWHERE R.uid = ' + uid + \
 	'ORDER BY title ASC;'
 	
 
 def popularReleaseParser(N):
-	return 'SELECT R.rid, R.title, R.reviewCount\nFROM Releases\nORDER BY reviewCount DESC\nLIMIT ' + N + ';'
+	return 'SELECT rid, title, reviewCount\nFROM releases\nORDER BY reviewCount DESC\nLIMIT ' + N + ';'
 
 
 def releaseTitleParser(sid):
-	return 'SELECT S.rid, S.release_title, S.genre, S.video_title, S.ep_num, S.length\n' + \
-	'FROM Sessions S\nWHERE S.sid = ' + sid + ';'
+	return 'SELECT rid, release_title, genre, video_title, ep_num, length\n' + \
+	'FROM sessions\nWHERE sid = ' + sid + ';'
 	
 
 def activeViewersParser(N, start, end):
 	return 'SELECT V.uid, V.firstname, V.lastname\n' + \
-	'FROM Viewers V\n' + \
-	'WHERE V.uid in (SELECT V1.uid\nFROM Viewers V1\nWHERE V1.initiate_at >= ' + start + ' AND V1.leave_at <= ' + end + ');'
+	'FROM viewers V\n' + \
+	'WHERE V.uid in (SELECT V1.uid\nFROM viewers V1\nWHERE V1.initiate_at >= "' + start + '" AND V1.leave_at <= "' + end + '");'
 
 
 def viewedVideosParser(rid):
     return ('SELECT V.rid, V.ep_num, V.title, V.length, COALESCE(view_counts.view_count, 0) AS view_count\n'
-            'FROM Videos V LEFT JOIN (SELECT S.rid, COUNT(DISTINCT S.uid) AS view_count FROM Sessions S WHERE S.rid = ' 
+            'FROM videos V LEFT JOIN (SELECT S.rid, COUNT(DISTINCT S.uid) AS view_count FROM sessions S WHERE S.rid = ' 
             + str(rid) + ' GROUP BY S.rid) view_counts ON V.rid = view_counts.rid WHERE V.rid = ' + str(rid) + ' ORDER BY V.rid DESC;')
