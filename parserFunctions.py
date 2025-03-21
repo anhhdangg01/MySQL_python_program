acefab4d846196136ecb5a116e82736e939fc22e
Tenzin Gaycha <Tgaycha@uci.edu>
2025-03-20 20:50:08 -0700

    updated PopularReleaseParser, fixed duplicates

--- parserFunctions.py
+++ parserFunctions.py
@@ -70,16 +70,6 @@ def releasesReviewedParser(uid):
 	'FROM releases Rel, reviews Rev\n' + \
 	'WHERE Rel.rid = Rev.rid AND Rev.uid = ' + uid + "\n" + \
 	'ORDER BY Rel.title ASC;'
-	
-
-def popularReleaseParser(N):
-	return 'SELECT Rel.rid, Rel.title, COUNT(Rev.rid) as reviewCount\n' \
-			'FROM releases Rel\n' \
-			'JOIN reviews Rev ON Rel.rid = Rev.rid\n' \
-			'GROUP BY Rel.rid, Rel.title\n' \
-			'HAVING reviewCount > 0\n' \
-			'ORDER BY reviewCount DESC\n' \
-			'LIMIT ' + N + ';'
 
 def popularReleaseParser(N):
     return 'SELECT Rel.rid, Rel.title, COUNT(Rev.rid) AS reviewCount\n'\
